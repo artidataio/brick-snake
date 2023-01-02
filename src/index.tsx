e2437ba13ea6500e2350ec07fddab7e84c40432e
Imaduddin Haetami <imaduddin.htm@gmail.com>
2023-01-02 16:39:20 +0700

    index.js to become index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
