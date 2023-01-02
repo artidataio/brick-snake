b7a495f77c52251b37fe82ae843278b3d55a4ac4
Imaduddin Haetami <imaduddin.htm@gmail.com>
2023-01-02 15:17:53 +0700

    Moving several constant and component into its separate file.

---
+++ src/Brick.tsx
@@ -0,0 +1,46 @@
+type Props = {
+  type: "apple" | "block" | "head";
+};
+
+export default function Brick(props: Props) {
+  let color;
+  switch (props.type) {
+    case "apple":
+      color = "red";
+      break;
+    case "block":
+      color = "brown";
+      break;
+    case "head":
+      color = "green";
+      break;
+    default:
+      color = "limegreen";
+  }
+
+  return (
+    <g>
+      {props.type === "apple" && (
+        <animate
+          attributeType="XML"
+          attributeName="opacity"
+          values="1;0"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      )}
+      <rect x="2.5" y="2.5" width="15" height="15" fill={color} />
+      <rect
+        x="0.25"
+        y="0.25"
+        width="19.5"
+        height="19.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="0.5"
+        rx="2.5"
+        ry="2.5"
+      />
+    </g>
+  );
+}
